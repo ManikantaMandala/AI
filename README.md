06d0c0d77464e94aef09f7afa7bfb0b175c2aee8
Manikanta Mandala <77623256+ManikantaMandala@users.noreply.github.com>
2022-11-19 08:15:41 +0000

    Started the initial coding work

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AI
 A repository for AI course in my University
+
+## Understanding on MiniMax Algorithm
+    - Backtracking algorithm
+        - Not breadth first algorithm
+    - Best move strategy used
+    - Max will try to maximize its utility.(Best Move)
+    - Min will try to minimize utility. (Worst Move)
+    - Worst-case time complexity is O(b^d).
+
+## Understanding on Alpha-beta pruning algorithm
+    - Optimized version of MiniMax algorithm.
+    - Cut off search by exploring less no. of nodes.
+    - Worst-case time complexity is O(b^(d/2)).
+    
---
+++ miniMaxReaching20.java
@@ -0,0 +1,22 @@
+class miniMaxReaching20{
+    public static void main(String args[]){
+        int result=0;
+        reaching20(result);
+    }
+    public static int addZero(int result){
+        //should check the previous player used the zero.
+        //And only once the player can use this in the whole game.
+        return result;
+    }
+    public static int addOne(int result){
+        return (result+1);
+    }
+    public static int addTwo(int result){
+        return (result+2);
+    }
+    public static int reaching20(int result){
+        //player should take a move.
+        //computer should take a move by alpha-beta pruning and print the results in the every iteration or the recursion!
+        return result;
+    }
+}
